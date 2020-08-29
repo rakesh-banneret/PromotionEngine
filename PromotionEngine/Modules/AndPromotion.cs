3233a577b171a0d3050603efcc091a871c068366
rakesh-banneret <rkumar24@live.com>
2020-08-29 16:35:09 +0530

    Added required Classes - Implementation is not yet done

---
+++ PromotionEngine/Modules/AndPromotion.cs
@@ -0,0 +1,15 @@
+﻿using PromotionEngine.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Modules
+{
+    public class AndPromotion : IPromotion
+    {
+        public double CalculatePrice(IDictionary<string, double> priceList, IDictionary<string, int> orderList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
