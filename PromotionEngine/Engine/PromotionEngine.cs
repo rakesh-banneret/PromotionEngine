a32ad9cd632a0a7d051fb3479e98253b15bc2d63
rakesh-banneret <rkumar24@live.com>
2020-08-29 17:17:15 +0530

    Added the Promotion Engine

---
+++ PromotionEngine/Engine/PromotionEngine.cs
@@ -0,0 +1,33 @@
+﻿using PromotionEngine.Contracts;
+using PromotionEngine.Modules;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace PromotionEngine.Engine
+{
+    public class PromotionEngine
+    {
+        ReadOnlyCollection<IPromotion> promotions;
+
+        public PromotionEngine() : this(new List<IPromotion> { new DefaultPromotion() }) { }
+
+        public PromotionEngine(IList<IPromotion> promotions)
+        {
+            this.promotions = new ReadOnlyCollection<IPromotion>(promotions);
+        }
+
+        public double CalculatePrice(IDictionary<string,double> priceList, IDictionary<string,int> orderList)
+        {
+            var tempOrder = new Dictionary<string, int>();
+            foreach(var item in orderList)
+            {
+                tempOrder[item.Key] = item.Value;
+            }
+
+            return promotions.Sum(a => a.CalculatePrice(priceList, tempOrder));
+        }
+    }
+}
