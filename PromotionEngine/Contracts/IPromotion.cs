c49b42f9887ee863011570f22faa1addd97fa5a3
rakesh-banneret <rkumar24@live.com>
2020-08-29 16:21:42 +0530

    Added Required projects

---
+++ PromotionEngine/Contracts/IPromotion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Contracts
+{
+    public interface IPromotion
+    {
+        double CalculatePrice(IDictionary<string, double> priceList, IDictionary<string, int> orderList);
+    }
+}
